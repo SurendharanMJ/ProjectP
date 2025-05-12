762f1f8154a88e4f952b84e7869f4a89871338e7
SurendharanMJ <surendharanmj909@gmail.com>
2025-05-12 13:21:31 +0530

    Changes done

--- Connection/database.js
+++ Connection/database.js
@@ -1,14 +1,3 @@
-// const mySQL = require('mysql2');
-
-// const DB = mySQL.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'cart',
-//     port: 3306
-
-// });
-// module.exports = DB.promise();
-
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("cart", "root", "", {
